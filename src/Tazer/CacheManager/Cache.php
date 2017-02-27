9468ca437c97efbe430395554338c11ae36f49e9
Patrik <rambo3swe@gmail.com>
2017-02-27 23:34:28 +0100

    :sparkles: inital commit added dependy on phpfastcache & predis , I will use phpfastcache for memory cache and predis for backplate pubsub

---
+++ src/Tazer/CacheManager/Cache.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tazer\CacheManager;
+
+class Cache
+{
+    public function __construct(){
+
+    }
+
+    public function sayHallo()
+    {
+        return 'Hallo!';
+    }
+}
---
+++ test/CacheTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tazer\CacheManager\Test\Cache;
+use Tazer\CacheManager\Cache;
+
+class StupidTest extends \PHPUnit_Framework_TestCase
+{
+	
+	public function testTrueIsTrue()
+	{
+        
+		$foo = true;
+		$this->assertTrue($foo);	
+	}
+
+    public function testCacheIsSayingHello()
+    {
+           $cache = new Cache();
+            $this->assertEquals($cache->sayHallo(), 'Hallo');
+    }
+
+
+	
+}
